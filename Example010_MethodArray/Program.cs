bc3b955b736817e2dfa8c818899412a1325acc00
DenNik <nikidenal@mail.ru>
2023-03-26 21:19:21 +0500

    Example010

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 20, 315, 689, 4, 52, 689, 714, 8};
+
+int n = array.Length;
+int find = 689;
+
+int index = 0;
+
+while (index < n)
+{
+    if(array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+        index++;
+}
