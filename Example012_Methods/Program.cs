d439418f5310fd8d378b5908695e7770d2882e29
DenNik <nikidenal@mail.ru>
2023-04-09 17:50:10 +0500

    Example013_RecursionAlgorithm

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -118,35 +118,35 @@
 // Упорядочить массив
 // сортировка или выбор макс мин
 
-int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
+// int[] arr = {1, 5, 4, 3, 2, 6, 7, 1, 1};
 
-void PrintArray(int[] array)
-{
-    int count = array.Length;
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
 
-    for (int i = 0; i < count; i++)
-    {
-        Console.Write($"{array[i]} ");
-    }
-    Console.WriteLine();
-}
+//     for (int i = 0; i < count; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.WriteLine();
+// }
 
-void SelectionSort(int[] array)
-{
-    for (int i = 0; i < array.Length - 1; i++)
-    {
-        int minPosition = i;
-        for (int j = i + 1; j < array.Length; j++)
-        {
-            if (array[j] < array[minPosition]) minPosition = j;
-        }
-        int temporary = array[i];
-        array[i] = array[minPosition];
-        array[minPosition] = temporary;
-    }
-}
+// void SelectionSort(int[] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int minPosition = i;
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if (array[j] < array[minPosition]) minPosition = j;
+//         }
+//         int temporary = array[i];
+//         array[i] = array[minPosition];
+//         array[minPosition] = temporary;
+//     }
+// }
 
-PrintArray(arr);
-SelectionSort(arr);
+// PrintArray(arr);
+// SelectionSort(arr);
 
-PrintArray(arr);
+// PrintArray(arr);
